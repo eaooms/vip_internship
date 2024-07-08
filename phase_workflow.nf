a9580d87bb991f6465a6e7b9bd21a33ccf06786e
ewoud <e.a.ooms@umcg.nl>
2024-07-08 07:11:21 +0000

    phase workflow

---
+++ phase_workflow.nf
@@ -0,0 +1,84 @@
+#!/usr/bin/env nextflow
+
+nextflow.enable.dsl=2
+
+params.reference = "$baseDir/resources/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+
+//Workflow_parameters 
+params.ont = ''
+params.ilmn = ''
+params.output = ''
+
+process RUN_CLAIR{
+    output:
+    stdout
+
+    script:
+    """
+    apptainer exec $baseDir/images/clairLatest.sif /opt/bin/run_clair3.sh \
+    --bam_fn=$params.ilmn \
+    --ref_fn=$params.reference \
+    --model_path /opt/models/ilmn \
+    --platform="ilmn" \
+    --threads=4 \
+    --output=$params.output/Ilmn.vcf
+    """
+}
+
+process WHATSHAPPHASE{
+    input:
+    stdin
+
+    output:
+    stdout
+
+    script:
+    """
+    whatshap phase -o $params.output/phased_short_with_long.vcf \
+    --ignore-read-groups --reference $params.reference $params.output/Ilmn.vcf $params.ont
+    """
+}
+process BGZIP{
+    input:
+    stdin
+
+    output:
+    stdout
+
+    script:
+    """
+    apptainer exec $baseDir/images/tabix.sif /opt/samtools/bin/bgzip $params.output/phased_short_with_long.vcf
+    """
+}
+
+process TABIX{
+    input:
+    stdin
+
+    output:
+    stdout
+
+    script:
+    """
+    apptainer exec $baseDir/images/tabix.sif /opt/samtools/bin/tabix $params.output/phased_short_with_long.vcf.gz
+    """
+}
+
+
+process WHATSHAPHAPLOTAG{
+    input:
+    stdin
+
+    output:
+    stdout
+
+    script:
+    """
+    whatshap haplotag -o $params.output/phased_haplo.bam \
+    --ignore-read-groups --reference $params.reference \
+    $params.output/phased_short_with_long.vcf.gz $params.ilmn
+    """
+}
+workflow {
+    RUN_CLAIR | WHATSHAPPHASE | BGZIP | TABIX | WHATSHAPHAPLOTAG
+}
---
+++ phase_workflow.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+SCRIPT_NAME="$(basename "$0")"
+
+# SCRIPT_DIR is incorrect when vip.sh is submitted as a Slurm job that is submitted as part of another Slurm job
+WORK_DIR="${WORK_DIR:-"${SCRIPT_DIR}"}"
+
+usage() {
+  echo -e "usage: ${SCRIPT_NAME} [-n <arg> -i <arg> -o <arg>]
+  -n, --nanopore             <arg>  path to nanopore bam file
+  -i, --illumina           <arg>  path to illumina bam file
+  -o, --output           <arg>  output folder"
+}
+
+validate() {
+  local -r nanopore="${1}"
+  local -r illumina="${2}"
+  local -r output="${3}"
+
+  if [[ -z "${nanopore}" ]]; then
+    >&2 echo -e "error: missing required -n / --input"
+    usage
+    exit 2
+  fi
+  if [[ ! -f "${nanopore}" ]] ; then
+    >&2 echo -e "error: input '${nanopore}' does not exist"
+    exit 2
+  fi
+
+  if [[ -z "${illumina}" ]]; then
+    >&2 echo -e "error: missing required -i / --input"
+    usage
+    exit 2
+  fi
+  if [[ ! -f "${illumina}" ]] ; then
+    >&2 echo -e "error: input '${illumina}' does not exist"
+    exit 2
+  fi
+
+  if [[ -z "${output}" ]]; then
+    >&2 echo -e "error: missing required -o / --output"
+    usage
+    exit 2
+  fi
+
+  # detect java, try to load module with name 'java' or 'Java' otherwise
+  if ! command -v java &> /dev/null; then
+    if command -v module &> /dev/null; then
+      if module is_avail java; then
+        module load java
+      elif module is_avail Java; then
+        module load Java
+      else
+        >&2 echo -e "error: missing required 'java'. could not find a module with name 'java' or 'Java' to load"
+        exit 2
+      fi
+    else
+      >&2 echo -e "error: missing required 'java'"
+      exit 2
+    fi
+  fi
+}
+
+execute_workflow(){
+  local -r paramNanopore="$(realpath "${1}")"
+  local -r paramIllumina="$(realpath "${2}")"
+  local -r paramOutput="$(realpath "${3}")"
+  local -r paramProfile="${4}"
+
+  $WORK_DIR/nextflow PhasingPipe.nf -c nextflow.config -profile $paramProfile --nanopore $paramNanopore --illumina $paramIllumina --output $paramOutput
+}
+
+main() {
+  local nanopore=""
+  local illumina=""
+  local output=""
+  local profile=""
+  if command -v sbatch &> /dev/null; then
+    profile="slurm"
+  else
+    profile="local"
+  fi
+  local args=$(getopt -o n:i:o: --long nanopore:,illumina:,output:,help: -- "$@")
+  eval set -- "${args}"
+
+  while :; do
+    case "$1" in
+    -n | --nanopore)
+      nanopore="$2"
+      shift 2 ;;
+    -i | --illumina )
+      illumina="$2"
+      shift 2 ;;
+    -o | --output)
+      output="$2"
+      shift 2 ;;
+    --)
+      shift
+      break ;;
+    *)
+      echo "Invalid option : $1"
+      exit 1 ;;
+    esac
+  done
+
+  validate "${nanopore}" "${illumina}" "${output}"
+
+  if ! [[ -d "${output}" ]]; then
+    mkdir -p "${output}"
+  fi
+
+  execute_workflow "${nanopore}" "${illumina}" "${output}" "${profile}"
+}
+
+main "${@}"
